97feb8ec62e6acf80bcb2e510aaa5b8b0e835136
sami-alex <samuel.alemayehu13.com>
2024-10-01 11:23:41 +0300

    finished features page

---
+++ src/data/features.ts
@@ -0,0 +1,30 @@
+export const features = [
+  {
+    imageUrl: "/iPhone 14 Plus.png",
+    title: "Fluid AI: Simplify, Organize, and Achieve More",
+    content:
+      "In a world where daily tasks, communication, and goals often become overwhelming, Fluid AI offers an innovative solution. Combining advanced AI technology with a user-friendly interface, it brings together to-do lists, chats, saved items, and bucket lists into one seamless platform.",
+    contentDirection: "left",
+  },
+  {
+    imageUrl: "/_Calendar_73.png",
+    title: "Fluid AI: Simplify, Organize, and Achieve More",
+    content:
+      "In a world where daily tasks, communication, and goals often become overwhelming, Fluid AI offers an innovative solution. Combining advanced AI technology with a user-friendly interface, it brings together to-do lists, chats, saved items, and bucket lists into one seamless platform.",
+    contentDirection: "right",
+  },
+  {
+    imageUrl: "/iPhone 14 Plus copy 5.png",
+    title: "Fluid AI: Simplify, Organize, and Achieve More",
+    content:
+      "In a world where daily tasks, communication, and goals often become overwhelming, Fluid AI offers an innovative solution. Combining advanced AI technology with a user-friendly interface, it brings together to-do lists, chats, saved items, and bucket lists into one seamless platform.",
+    contentDirection: "left",
+  },
+  {
+    imageUrl: "/iPhone 14 Plus copy 4.png",
+    title: "Fluid AI: Simplify, Organize, and Achieve More",
+    content:
+      "In a world where daily tasks, communication, and goals often become overwhelming, Fluid AI offers an innovative solution. Combining advanced AI technology with a user-friendly interface, it brings together to-do lists, chats, saved items, and bucket lists into one seamless platform.",
+    contentDirection: "right",
+  },
+];
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <body style={{ margin: 0 }}>
+      <body style={{ margin: 0, backgroundColor: "black" }}>
         <Main />
         <NextScript />
       </body>
