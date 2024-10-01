f6259aee6092ee9233a94d71bbc4ea1b8357f117
sami-alex <samuel.alemayehu13.com>
2024-10-01 14:20:15 +0300

    finished vision section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import HeroSection from "@/sections/landingPage/hero";
 import AboutUsSection from "@/sections/landingPage/features";
 import { Container } from "@mui/material";
+import VisionSection from "@/sections/landingPage/vision";
 export default function Home() {
   return (
     <>
+      <HeroSection />
       <Container maxWidth="xl">
-        <HeroSection />
         <AboutUsSection />
       </Container>
+      <VisionSection />
     </>
   );
 }
